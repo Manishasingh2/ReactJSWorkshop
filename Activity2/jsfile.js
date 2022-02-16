5f8a76a96d69758f312a22d97286ff625a1236bf
Manisha Singh <manisha.singh@fareportal.com>
2022-02-17 00:08:36 +0530

    second commmit

---
+++ Activity2/jsfile.js
@@ -0,0 +1,21 @@
+function mouseOver1()
+{
+    document.getElementById("div1").style.color = "red";
+    document.getElementById("div1").style.textAlign="center";
+}
+function mouseOver2()
+{
+    document.getElementById("div2").style.color = "red";
+    document.getElementById("div2").style.textAlign="center";
+}
+  
+function mouseOut1()
+{
+    document.getElementById("div1").style.color = "black";
+    document.getElementById("div1").style.textAlign="left";
+}
+function mouseOut2()
+{
+    document.getElementById("div2").style.color = "black";
+    document.getElementById("div2").style.textAlign="left";
+}
