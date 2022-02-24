883771a3a8d6a2a9c519264b7826694a2c8eefd2
Manisha Singh <manisha.singh@fareportal.com>
2022-02-25 00:35:01 +0530

    ClassComponentpostreq

--- activitypostreq/src/classComponent/AddEmployee.js
+++ activitypostreq/src/classComponent/AddEmployee.js
@@ -7,25 +7,23 @@ export default class AddEmployee extends React.Component {
     salary: 0,
     designation: ""
   };
-  handleChange = (event) => {
-
+  handleIdChange = (event) => {
     this.setState({
       eid: event.target.value
     });
   };
-  handleChange2 = (event) => {
+  handleNameChange = (event) => {
     this.setState({
       ename: event.target.value
     });
   };
-  handleChange3 = (event) => {
+  handleSalaryChange = (event) => {
     this.setState({
       salary: event.target.value
 
     });
   };
-  handleChange4 = (event) => {
-
+  handleDesignationChange = (event) => {
     this.setState({
       designation: event.target.value
 
@@ -34,13 +32,7 @@ export default class AddEmployee extends React.Component {
   };
   handleSubmit = event => {
     event.preventDefault();
-    const user = {
-        eid: this.state.eid,
-        ename: this.state.ename,
-        salary: this.state.salary,
-        designation: this.state.designation
-    };
-   axios.post("https://localhost:5001/api/Employee", {user})
+   axios.post("https://localhost:5001/api/Employee/postemp", {eid:this.state.eid,ename:this.state.ename,salary:this.state.salary,designation:this.state.designation})
       .then(res =>
         console.log(res.data))
       .catch(err => console.log(err))
@@ -49,28 +41,29 @@ export default class AddEmployee extends React.Component {
   render() {
     return (
       <>
-        <form className="row g-3" onSubmit={this.handleSubmit} >
+        <h4 style={{color:"blue"}}>Add A New EMPLOYEE</h4>
+        <form className="row g-3" >
           <div className="col-md-2">
             <label className="form-label">Employee ID</label>
-            <input className="form-control" value={this.state.eid} onChange={this.handleChange} />
+            <input className="form-control" style={{width:"250px"}} value={this.state.eid} onChange={this.handleIdChange} />
           </div>
 
-          <div className="col-12">
+          <div>
             <label className="form-label">Employee Name</label>
-            <input className="form-control" value={this.state.ename} onChange={this.handleChange2} />
+            <input className="form-control" style={{width:"250px"}} value={this.state.ename} onChange={this.handleNameChange} />
           </div>
-          <div className="col-md-6">
+          <div>
             <label className="form-label">Salary</label>
-            <input className="form-control" value={this.state.salary} onChange={this.handleChange3} />
+            <input className="form-control" style={{width:"250px"}} value={this.state.salary} onChange={this.handleSalaryChange} />
           </div>
 
-          <div className="col-md-6">
+          <div >
             <label className="form-label">Designation</label>
-            <input className="form-control" value={this.state.designation} onChange={this.handleChange4} />
+            <input className="form-control" style={{width:"250px"}} value={this.state.designation} onChange={this.handleDesignationChange} />
           </div>
 
           <div className="col-12">
-            <button type="submit" className="btn btn-primary" >Add Employee</button>
+            <button type="submit" className="btn btn-primary" style={{width:"250px"}} onClick={this.handleSubmit}>Add Employee</button>
           </div>
         </form>
       </>
