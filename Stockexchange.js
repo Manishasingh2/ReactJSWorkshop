2042ba2889517cd2a28c416c1845c762068c9379
Manisha Singh <manisha.singh@fareportal.com>
2022-02-26 13:45:13 +0530

    stock exchange activity using useEffect

---
+++ Stockexchange.js
@@ -0,0 +1,19 @@
+import Axios from "axios";
+import React from "react";
+import { useState,useEffect } from "react";
+export default function Stockexchange()
+{   debugger;
+    var [price,setPrice]=useState(0);
+    useEffect(()=>{
+        var dataPromise = Axios.get("https://priceapi.moneycontrol.com/pricefeed/bse/equitycash/SBI");
+        dataPromise.then((response) => {
+            debugger;
+        setPrice(response.data.data);
+        })
+    },[])
+    return(
+        <div>
+            <h1>Current Price:{price.pricecurrent}</h1>   
+        </div>
+    )   
+}
